8bb710a44849133f6b269c24566506ade39dc7db
hmj2622019Nobumoto <hmj2622019@stu.o-hara.ac.jp>
2026-07-03 12:27:36 +0900

    Run

--- Run.cpp
+++ Run.cpp
@@ -1,1 +1,29 @@
 #include "DxLib.h"
+
+const int WIDTH = 960, HEIGHT = 640;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdline, int nCmdShow)
+{
+{
+
+	SetGraphMode(WIDTH, HEIGHT, 32);
+	ChangeWindowMode(true);
+	if (DxLib_Init() == -1) return -1;
+	SetDrawScreen(DX_SCREEN_BACK);
+	//while (1)
+	//{
+	//	ClearDrawScreen();
+
+
+
+
+
+	//	ScreenFlip();
+	//	WaitTimer(16);
+	//	if (ProcessMessage() == -1) break;
+	//	if (CheckHitKey(KEY_INPUT_ESCAPE) == 1) break;
+	//}
+
+	DxLib_End();
+	return 0;
+}
